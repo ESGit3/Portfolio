fefeedad216e16339cdcaf59b63a773ae76c16e3
ESGit3 <ethanshan706@gmail.com>
2020-01-31 07:57:00 -0500

    nav and centerContainer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <header className="App-header">
+                <nav>
+                  <h1>Portfolio</h1>
+                  <ul className="navList">
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+            </header>
+        <section className="splash">
+          <div className="centerContainer">
+              <h1>Ethan Shan</h1>
+              <h6>test</h6>
+          </div>
+        </section>
     </div>
   );
 }
