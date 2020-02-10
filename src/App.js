dbd9f24a49c6a49703a0b04379ad90a8d6011300
ESGit3 <ethanshan706@gmail.com>
2020-02-10 12:32:00 -0500

    about start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -19,6 +21,22 @@ function App() {
               <h1>Ethan Shan</h1>
               <h6>test</h6>
           </div>
+        </section>
+        <section className="About">
+            <div className="row">
+                <div className="col-1"/>
+                <div className="col-10">
+                    <div className="row">
+                        <div className="col-7">
+                            <h4>lmao this is dumb</h4>
+                        </div>
+                        <div className="col-3">
+                            <img src="assets/Splash.jpg" alt=""/>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-1"/>
+            </div>
         </section>
     </div>
   );
